49e71654cdd7ffb5d18c089b0ebe85cf9fa79646
Nathan Giesbrecht <44386693+NateGiesbrecht@users.noreply.github.com>
2019-06-25 21:53:01 -0500

    Fixed output

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class Main {
 
-        public static int rolls;
-        public static int sides;
-        public static int index;
+        private static int rolls;
+        private static int sides;
+        private static int index;
 
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
@@ -20,9 +20,9 @@ public class Main {
             rollDice(rolls,sides);
 
         }
-        public static void rollDice(int rolls, int sides){
+        private static void rollDice(int rolls, int sides){
             int sum = 0;
-            System.out.println("rolls : "+ rolls + "sides : " + sides);
+            System.out.println("rolls : " + rolls + "sides : " + sides);
             for(int i = 0; i < rolls; i++){
                 int roll = 1+ (int)(Math.random() * ( sides  ));
                 System.out.println("You rolled : " + roll);
@@ -31,7 +31,7 @@ public class Main {
             System.out.println("Total roll was : " + sum);
         }
 
-        public static int indexOfD(String input){
+        private static int indexOfD(String input){
             int index = 0;
             for(int i = 0; i < input.length(); i++){
                 if (input.charAt(i) == 'd')
@@ -40,7 +40,7 @@ public class Main {
             return index;
         }
 
-        public static int howManySides(int index, String input){
+        private static int howManySides(int index, String input){
             String sides = "";
             System.out.println(index);
             for(int i = index+1; i < input.length(); i++){
@@ -49,7 +49,7 @@ public class Main {
             return Integer.parseInt(sides);
         }
 
-    public static int howManyRolls(int index, String input){
+    private static int howManyRolls(int index, String input){
         String rolls = "";
         for(int i = 0; i < index; i++){
             rolls = rolls + input.charAt(i);
